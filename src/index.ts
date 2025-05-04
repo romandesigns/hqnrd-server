341bf06b02cfc4a0cda2af2eba7ec5c245e86f8e
Roman F. <wavystyledev@gmail.com>
2025-05-04 09:55:55 -0400

    proof of concept

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
 import { Elysia } from "elysia";
 import { ConvexClient } from "convex/browser";
 import { api } from "../convex/_generated/api.js";
+import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia();
+// App port
+const PORT = Bun.env.PORT || 32000;
 const { log } = console;
+// Initializing Elysia server
+const app = new Elysia();
+// Swagger documentation
+app.use(swagger());
 
-app.get("/", async () => {
+app.get("/api/tasks", async () => {
   const client = new ConvexClient(Bun.env.CONVEX_URL!);
   const tasks = await client.query(api.tasks.getTasks, {});
   return tasks;
 });
 
-app.listen(3000, () =>
+app.listen(PORT, () =>
   log(`🦊 App running at ${app.server?.hostname}:${app.server?.port}`)
 );
